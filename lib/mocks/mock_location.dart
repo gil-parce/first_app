d905505fb602dfa2e1e05befc4269b381842655d
gil-parce <gilselby@gmail.com>
2021-01-14 18:32:23 +0100

    Adds image and formats title and text sections

---
+++ lib/mocks/mock_location.dart
@@ -0,0 +1,20 @@
+import 'package:first_app/models/location.dart';
+import 'package:first_app/models/location_fact.dart';
+
+class MockLocation extends Location {
+  static Location FetchAny() {
+    return Location(
+        name: 'Arashiyama Bamboo Grove, Kyoto, Japan',
+        url: 'https://www.touristinjapan.com/wp-content/uploads/2019/03/arashiyama-bamboo-forest-1020x600.jpg',
+        facts: <LocationFact>[
+          LocationFact(
+              title: 'Summary',
+              text:
+              'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm'),
+          LocationFact(
+              title: 'How to Get There',
+              text:
+              'Kyoto airport, with several terminals, is located 16 kilometres south of the city and is also known as Kyoto. Kyoto can also be reached by transport links from other regional airports.')
+        ]);
+  }
+}
