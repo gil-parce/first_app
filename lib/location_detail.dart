ae647e9a3eeb24860a5b46fef2a78734913a4047
gil-parce <gilselby@gmail.com>
2021-01-19 17:47:49 +0100

    Adds custom font for the app's text

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/location.dart';
+import 'styles.dart';
 
 class LocationDetail extends StatelessWidget {
   final Location location;
@@ -9,7 +10,8 @@ class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(location.name)),
+        appBar: AppBar(title: Text(location.name, style:
+        Styles.navBarTitle)),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -38,16 +40,13 @@ class LocationDetail extends StatelessWidget {
         padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
         child: Text(text,
       textAlign: TextAlign.left,
-      style: TextStyle(
-        fontSize: 25.0,
-        color: Colors.black,
-      )));
+      style: Styles.headerLarge));
   }
 
   Widget _sectionText(String text) {
     return Container(
         padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
-        child: Text(text));
+        child: Text(text, style: Styles.textDefault));
   }
 
   Widget _bannerImage(String url, double height) {
